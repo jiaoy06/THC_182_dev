efff9f16ba0ab63eeab1e66b55d66c2b4186aa5c
jiaoy06 <jiaoyangchaming@gmail.com>
2014-09-10 22:48:28 +0800

    Tools: to thumbnail
    Add a python script to resize card pic to thumbnail pic.

---
+++ tools_Gen/picn/to_thumbnail.py
@@ -0,0 +1,16 @@
+﻿# coding='utf-8'
+
+import glob
+import os
+import Image
+
+jpgNameList = glob.glob('*.jpg')
+outDir = 'thumbnail'
+
+for jpgName in jpgNameList:
+    print jpgName
+    im = Image.open(jpgName)
+    print im.size
+    # 滤镜缩放
+    om = im.resize((45, 64), Image.ANTIALIAS)
+    om.save(os.path.join(outDir, jpgName))
