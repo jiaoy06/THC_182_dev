6403e2f0182ea00443a4e73a63611df6e7f7562c
jiaoy06 <jiaoyangchaming@gmail.com>
2014-09-15 15:07:15 +0800

    Effect: c198800001 cannot be destroyed (I)
    Add Effect: cannot be destroyed.
    Bugs: can be destroyed if in a chain.
    not finished.

--- script/c198800001.lua
+++ script/c198800001.lua
@@ -1,33 +1,26 @@
 require "nef/dss"
 --东方 M·U·G·E·N
-function c10000.initial_effect(c)
+function c198800001.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--defup
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_UPDATE_DEFENCE)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x208))
-	e2:SetValue(600)
-	c:RegisterEffect(e2)
 	--Destroy replace
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCondition(c10000.desrepcon)
+	-- e3:SetCondition(c198800001.desrepcon)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
+	-- TODO
+	--lv up and changed to set0x6208 
 end
-function c10000.filter1(c)
-	return c:IsFaceup() and c:IsSetCard(0x208)
-end
-function c10000.desrepcon(e)
-	return Duel.IsExistingMatchingCard(c10000.filter1,e:GetHandler():GetControler(),LOCATION_MZONE,LOCATION_MZONE,1,nil)
-end
+-- function c198800001.filter1(c)
+-- 	return c:IsFaceup() and c:IsSetCard(0x208)
+-- end
+-- function c198800001.desrepcon(e)
+-- 	return Duel.IsExistingMatchingCard(c198800001.filter1,e:GetHandler():GetControler(),LOCATION_MZONE,LOCATION_MZONE,1,nil)
+-- end
