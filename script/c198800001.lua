e3cdc7e04e17a89795c0026fbe452b931a5046f2
jiaoy06 <jiaoyangchaming@gmail.com>
2014-09-17 01:06:36 +0800

    Scripts: finish c198800001
    Finished and simply tested the script of c198800001 （东方mugen）

--- script/c198800001.lua
+++ script/c198800001.lua
@@ -1,26 +1,87 @@
 require "nef/dss"
---东方 M·U·G·E·N
+-- 东方✿M·U·G·E·N
 function c198800001.initial_effect(c)
-	--Activate
+	-- 发动
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--Destroy replace
+	-- 不能被破坏
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	-- lv上升4，当作黑暗调整
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetCategory(CATEGORY_LVCHANGE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetRange(LOCATION_SZONE)
-	-- e3:SetCondition(c198800001.desrepcon)
-	e3:SetValue(1)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c198800001.target)
+	e3:SetOperation(c198800001.activate)
 	c:RegisterEffect(e3)
-	-- TODO
-	--lv up and changed to set0x6208 
 end
--- function c198800001.filter1(c)
--- 	return c:IsFaceup() and c:IsSetCard(0x208)
--- end
--- function c198800001.desrepcon(e)
--- 	return Duel.IsExistingMatchingCard(c198800001.filter1,e:GetHandler():GetControler(),LOCATION_MZONE,LOCATION_MZONE,1,nil)
--- end
+
+function c198800001.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x208) and not c:IsType(TYPE_XYZ)
+end
+
+function c198800001.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c198800001.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c198800001.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,c198800001.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+
+function c198800001.synlimit(e,c)
+	if not c then return false end
+	if c:IsSetCard(0x5208) then
+	return else return not c:IsSetCard(0x120) end
+end
+
+function c198800001.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		-- lv上升4
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+RESET_END)
+		e1:SetValue(4)
+		tc:RegisterEffect(e1)
+
+		-- 当作调整
+		if not tc:IsType(TYPE_TUNER) then
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_ADD_TYPE)
+			e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+RESET_END)
+			e2:SetValue(TYPE_TUNER)
+			tc:RegisterEffect(e2)
+		end
+
+		-- 当做黑暗调整使用
+		-- 目前手段：当成【M·U·G·E·N✿黑暗调整衍生物】的同名卡使用
+		local code=198800099
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e3:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+RESET_END)
+		e3:SetCode(EFFECT_CHANGE_CODE)
+		e3:SetValue(code)
+		tc:RegisterEffect(e3)
+
+		-- 黑暗调整不能作为普通同调怪兽的素材
+		local e4=Effect.CreateEffect(c)
+		e4:SetType(EFFECT_TYPE_SINGLE)
+		e4:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+		e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+		e4:SetValue(c198800001.synlimit)
+		tc:RegisterEffect(e4)
+	end
+end
